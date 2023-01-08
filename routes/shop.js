12fcd65e2400321b964ec9e8980ff08d05819590
Sandip Rode <16761010+Sandip1447@users.noreply.github.com>
2023-01-08 17:51:21 +0530

    Enhancing the App

--- routes/shop.js
+++ routes/shop.js
@@ -6,12 +6,13 @@ const shopController = require('../controllers/shop')
 
 const router = express.Router();
 
-// Need to create a path for adding shop.html file from views folder
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
